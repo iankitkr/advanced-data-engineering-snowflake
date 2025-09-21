2bccab9ef89019d5a6e4dde89efc4f8d3ba67eac
Ankit Kumar <ankitkumar@192.168.1.11>
2025-09-21 23:04:59 +0530

    update in file

--- module-1/api_integration.sql
+++ module-1/api_integration.sql
@@ -1,25 +1,26 @@
 USE ROLE accountadmin;
-CREATE DATABASE course_repo;
+drop DATABASE course_repo;
+create DATABASE course_repo;
 USE SCHEMA public;
 
 -- Create credentials
 CREATE OR REPLACE SECRET course_repo.public.github_pat
   TYPE = password
-  USERNAME = ''
-  PASSWORD = '';
-
+  USERNAME = 'iankitkr'
+  PASSWORD = ''
+;
 -- Create the API integration
-CREATE OR REPLACE API INTEGRATION
+CREATE OR REPLACE API INTEGRATION git_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('') -- URL to your GitHub profile
-  ALLOWED_AUTHENTICATION_SECRETS = ()
+  API_ALLOWED_PREFIXES = ('https://github.com/iankitkr')
+  ALLOWED_AUTHENTICATION_SECRETS = (course_repo.public.github_pat) -- allow your secret
   ENABLED = TRUE;
 
 -- Create the git repository object
 CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
-  API_INTEGRATION =  -- Name of the API integration defined above
-  ORIGIN = '' -- Insert URL of forked repo
-  GIT_CREDENTIALS = ;
+  API_INTEGRATION =  git_integration-- Name of the API integration defined above
+  ORIGIN = 'https://github.com/iankitkr/advanced-data-engineering-snowflake.git' -- Insert URL of forked repo
+  GIT_CREDENTIALS = course_repo.public.github_pat;
 
 -- List the git repositories
 SHOW GIT REPOSITORIES;
